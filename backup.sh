eb496332d33e14fc7ccc0453a8aeec501695c229
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2015-06-30 22:16:54 -0400

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -7,4 +7,6 @@ mysqldump -u root -pmysqlpsswd --all-databases > /var/backups/alldb_backup.sql
 cp  /etc/hosts  /var/backups/
 
 #Backup importand files relate to app
-
+cp -R /var/www/dcim/pictures /var/backups/pictures
+cp -R /var/www/dcim/drawings /var/backups/drawings
+cp -R /var/www/dcim/images /var/backups/images
