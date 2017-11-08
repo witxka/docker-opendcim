26ce14b2616c7d203c52d4d9432c828a04fe1221
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2017-11-08 15:52:06 -0500

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -2,12 +2,12 @@
 
 mkdir -p /var/run/mysqld
 chown mysql:mysql /var/run/mysqld /var/lib/mysql
-chpst -u mysql /usr/bin/mysqld_safe --initialize-insecure --skip-grant-tables &
+chpst -u mysql /usr/bin/mysqld --initialize --skip-grant-tables &
+sleep 2s
+password=$(cat /var/log/mysql/error.log | grep "A temporary password is generated for" | tail -1 | sed -n 's/.*root@localhost: //p')
 
- sleep 5s
-
- chpst -u mysql /usr/bin/mysqld_safe  & 
- mysqladmin -u root password mysqlpsswd
+ mysql -uroot -p$password -Bse "ALTER USER 'root'@'localhost' IDENTIFIED BY 'mysqlpsswd';"
+ #mysqladmin -u root password mysqlpsswd
  mysqladmin -u root -pmysqlpsswd reload
  mysqladmin -u root -pmysqlpsswd create dcim
 
