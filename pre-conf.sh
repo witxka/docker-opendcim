c5c22ce1d47b1516e48917de8096abdcba944dd4
Anshu Prateek <anshprat@users.noreply.github.com>
2018-08-30 18:45:34 +0530

    Update openDCIM to 18.01

--- pre-conf.sh
+++ pre-conf.sh
@@ -2,7 +2,7 @@
 
 /usr/bin/mysqld_safe  &
 sleep 2s
- 
+opendcim_version=18.01
  mysqladmin -u root password mysqlpsswd
  mysqladmin -u root -pmysqlpsswd reload
  mysqladmin -u root -pmysqlpsswd create dcim
@@ -11,28 +11,28 @@ sleep 2s
  echo "SET GLOBAL sql_mode = '';" | mysql -u root -pmysqlpsswd
 
  cd /var/www
- wget http://opendcim.org/packages/openDCIM-4.5.tar.gz
- tar zxpvf openDCIM-4.5.tar.gz
- ln -s openDCIM-4.5 dcim
- rm openDCIM-4.5.tar.gz
+ wget http://opendcim.org/packages/openDCIM-${opendcim_version}.tar.gz
+ tar zxpvf openDCIM-${opendcim_version}.tar.gz
+ ln -s openDCIM-${opendcim_version} dcim
+ rm openDCIM-${opendcim_version}.tar.gz
  rm -R /var/www/html
  chgrp -R www-data /var/www/dcim/pictures /var/www/dcim/drawings /var/www/dcim/vendor/mpdf/mpdf/ttfontdata
- 
+
  cd /var/www/dcim
  cp db.inc.php-dist db.inc.php
- 
+
  #to fix error relate to ip address of container apache2
  echo "ServerName localhost" | tee /etc/apache2/conf-available/fqdn.conf
  ln -s /etc/apache2/conf-available/fqdn.conf /etc/apache2/conf-enabled/fqdn.conf
- 
- #  copy conf of 
+
+ #  copy conf of
  cat << EOF > /var/www/dcim/.htaccess
 AuthType Basic
 AuthName "openDCIM"
 AuthUserFile /var/www/opendcim.password
 Require valid-user
 EOF
- 
+
  htpasswd -cb /var/www/opendcim.password dcim dcim
  a2enmod rewrite
 
