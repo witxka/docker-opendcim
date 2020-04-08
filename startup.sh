efc134c2f3f49219da824f5bff5d6761e55c8729
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2020-04-08 13:02:35 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -7,21 +7,21 @@ set -e
 if [ "$(ls -A /var/lib/mysql)" ]; then
     echo "mysql folder with data"    
 else
-    cp -Rp /var/backup/mysql/* /var/lib/mysql/ 
+    cp -Rp /var/backup/mysql/. /var/lib/mysql/ 
     chown mysql:mysql /var/lib/mysql
 fi
 
 if [ "$(ls -A /var/www/dcim/pictures)" ]; then
    echo "pictures folder with data"
 else
-    cp -Rp /var/backup/pictures/* /var/www/dcim/pictures/
+    cp -Rp /var/backup/pictures/. /var/www/dcim/pictures/
     chown www-data:www-data /var/www/dcim/pictures
 fi
 
 if [ "$(ls -A /var/www/dcim/drawings)" ]; then
    echo "drawings folder with data"
 else
-    cp -Rp /var/backup/drawings/* /var/www/dcim/drawings/
+    cp -Rp /var/backup/drawings/. /var/www/dcim/drawings/
     chown www-data:www-data /var/www/dcim/drawings
 fi
 
